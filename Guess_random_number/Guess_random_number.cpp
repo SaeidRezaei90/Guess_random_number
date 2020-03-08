bcbd4c9c1899a0fe19738175b39617103df3331c
Saeid <46779198+SaeidRezaei90@users.noreply.github.com>
2020-03-09 00:37:49 +0100

    new changes

--- Guess_random_number/Guess_random_number.cpp
+++ Guess_random_number/Guess_random_number.cpp
@@ -9,20 +9,28 @@
 #include <limits>
 #include <vector>
 #include <array>
+#include <fstream>
 
 using std::cout;
 using std::cin;
 using std::endl;
-void Play_Game(); //declare
+using std::string;
+void Play_Game(); //declaration the method
 
-void Print_array(int guess_array[], int array_size)
+std::string fileName = "high-score.txt";
+std::ifstream readFile(fileName);
+std::ofstream writeFile(fileName);
+int best_Score = 9999;
+
+void Print_array(int guess_array[], int array_size, int best_score)
 {
 	std::cout << "You entered the following numbers:\n";
 	for (int i = 0; i < array_size; i++)
 	{
 		std::cout << guess_array[i] << "\t";
 	}
-	std::cout <<std::endl << "You guessed " << array_size << " numbers and " << guess_array[array_size - 1] << " is the right choice\n \n";
+	std::cout << std::endl << "You guessed " << array_size << " numbers and " << guess_array[array_size - 1] << " is the right choice\n"
+							  "Your best score is "<<best_score <<std::endl << std::endl << std::endl;
 }
 
 
@@ -77,15 +85,15 @@ void Play_Game()
 			if (random_guess_array[array_size] == random)
 				std::cout << "\n \n Congratulation, You win\n";
 			else if (random_guess_array[array_size] < random)
-				std::cout << "You enter a low number\n";
+				std::cout << "You entered a low number\n";
 			else
-				std::cout << "You enter a high number\n";
-			
+				std::cout << "You entered a high number\n";
+
 			array_size++;
 		}
 		else
 		{
-			std::cout << "Please enter a valid number:\n";
+			std::cout << "Please entered a valid number:\n";
 			cin.clear();
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			continue;
@@ -94,9 +102,26 @@ void Play_Game()
 	} while (random_guess_array[array_size - 1] != random && array_size < 10);
 
 	if (random_guess_array[array_size - 1] == random)
-		Print_array(random_guess_array, array_size);
+	{
+		if (!readFile.is_open())
+		{
+			std::cout << "Your file is not valid\n";
+			exit(1);
+		}
+		else
+		{
+			readFile >> best_Score;
+		}
+
+		if (array_size < best_Score)
+		{
+			best_Score = array_size;
+			writeFile << array_size;
+		}
+		Print_array(random_guess_array, array_size, best_Score);
+	}
 	else
-		std::cout << "I'm sorry, you had 10 guesses but you didn't guess correctly" << std::endl;
+		std::cout << "I'm sorry, you had 10 guesses but you didn't guess correctly" << std::endl << std::endl;
 
 	if (Play_or_Not() == false)
 		exit(0);
@@ -106,11 +131,10 @@ void Play_Game()
 int main()
 {
 	srand(time(NULL));
+	writeFile << best_Score;
 
 	if (Play_or_Not() == false)
 		return 0;
-
-	
 	_getch();
 	return 0;
 }
